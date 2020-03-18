239e4076804e5bb5dc9435559b8770416c4ae921
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-03-18 15:38:23 -0700

    add DailyReport and variables_and_data_types projects

---
+++ basic_c_sharp_projects/DailyReport/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy\nStudent Daily Report");
+            Console.WriteLine("What course are you on?");
+            string studentCourseInput = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string studentPageInput = Console.ReadLine();
+            short PageNumber = Convert.ToInt16(studentPageInput);
+            Console.WriteLine("Do you need help with anything? Please answer “true” or “false”");
+            string studentHelpInput = Console.ReadLine();
+            bool needsHelp = Convert.ToBoolean(studentHelpInput);
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            string positiveExperiences = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            string otherFeedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string studentHoursInput = Console.ReadLine();
+            float hoursStudied = float.Parse(studentHoursInput);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.Read();
+
+
+
+        }
+    }
+}
---
+++ basic_c_sharp_projects/variables_and_data_types/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace variables_and_data_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // THis is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is " + yourName);
+            //Console.Read();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your Favorite Number plus 5 is: " + total);
+            //Console.Read();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemp = -23;
+            char questionMark = '\u2103';
+            
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimeters = 211.302032030;
+
+            float secondsLeft = 2.62f;
+
+            short tempOnMars = -341;
+
+            string myName = "Jon";
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+
+            Console.WriteLine(rainingStatus);
+            Console.Read();
+
+        }
+    }
+}
