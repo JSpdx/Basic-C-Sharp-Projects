744da61a1c53cb62eb25624ff56375c2827057fd
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-03-20 18:43:22 -0700

    add count to 100

---
+++ basic_c_sharp_projects/CountToOneHundred/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CountToOneHundred
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 0;
+            while ( x < 100)
+            {
+                x += 1;
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("Hit Enter to go back down");
+            Console.ReadLine();
+            
+            do
+            {
+                x -= 1;
+                Console.WriteLine(x);
+            }
+            while (x > 0);
+            Console.ReadLine();
+
+        }
+
+    }
+}
