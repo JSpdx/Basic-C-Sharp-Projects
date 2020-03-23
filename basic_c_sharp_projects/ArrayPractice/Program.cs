e47fe095ff9880258b9fce58d9d1a4ebb712d0f9
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-03-23 12:56:32 -0700

    add ArrayPractice

---
+++ basic_c_sharp_projects/ArrayPractice/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPractice
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] progArray = new string[4];
+            progArray[0] = "\nYES";
+            progArray[1] = "\nRush";
+            progArray[2] = "\nKing Crimson";
+            progArray[3] = "\nFrank Zappa";
+
+            int[] intArray = new int[3];
+            intArray[0] = 123;
+            intArray[1] = 5234;
+            intArray[2] = 412;
+
+            bool appExit = false;
+            while (appExit == false)
+            {
+                try
+                {
+                    Console.WriteLine("Select a number between 0 and 3:");
+                    int i = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(progArray[i]);
+                    
+                    Console.WriteLine("\nPick a number between 0 and 2:");
+                    i = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(intArray[i]);
+                    
+                    appExit = true;
+                }
+                catch (System.IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Oops! Looks like your number was out of range. Try again!");
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Oops! You didn't enter anything. Try again!\n");
+                }
+            }
+            Console.ReadLine();
+            System.Environment.Exit(0);
+        }
+    }
+}
---
+++ basic_c_sharp_projects/ArraysAndLists/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> intList = new List<string>();         
+            intList.Add("hi");
+            intList.Add("whats up");
+            intList.Add("not much");
+            intList.Remove("not much");
+            
+            Console.WriteLine(intList[1]);
+            Console.ReadLine();
+
+            
+            //int[] numArray = new int[5];
+            //numArray[0] = 5;
+            //numArray[1] = 2;
+            //numArray[2] = 10;
+            //numArray[3] = 200;
+            //numArray[4] = 5000;
+
+
+
+            //int[] numArray1 = new int[] { 5, 5, 10, 200, 5000 };
+
+            //int[] numArray2 = { 5, 4, 10, 200, 5000, 45, 456, 234 };
+            //numArray2[5] = 670;
+            //Console.WriteLine(numArray2[5]);
+            //Console.ReadLine();
+
+
+        }
+    }
+}
