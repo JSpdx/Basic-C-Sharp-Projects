a27996a2ed87bfb3238bea795a5ee1bea4042075
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-03-25 11:58:52 -0700

    add ClassMethodExercise2

---
+++ basic_c_sharp_projects/ClassMethodExercise2/Operations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodExercise2
+{
+    class Operations
+    {
+        public int mathStuff(int x)
+        {
+            return x * x;
+        }
+
+        public decimal mathStuff(int x, decimal y)
+        {
+            return x * x * x;
+        }
+
+        public int mathStuff(int x, decimal y, string z)
+        {
+            int convertedInt = Convert.ToInt32(z);
+            int result = convertedInt * 42;
+            return result;
+        }
+
+
+    }
+}
---
+++ basic_c_sharp_projects/ClassMethodExercise2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operations operations1 = new Operations();
+            Console.WriteLine(operations1.mathStuff(3));
+
+            Operations operations2 = new Operations();
+            Console.WriteLine(operations2.mathStuff(3, 23.32m));
+
+            Operations operations3 = new Operations();
+            Console.WriteLine(operations3.mathStuff(5, 23.6m, "34"));
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
