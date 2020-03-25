95d220cda8500ac968cf882a414e30a928cf7242
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-03-25 14:31:41 -0700

    add ClassMethodExercise4

---
+++ basic_c_sharp_projects/ClassMethodExercise4/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodExercise4
+{
+    class Class1
+    {
+        public void stuff(int first, int second)
+        {
+            first = first / 2;
+            Console.WriteLine(second);
+        }
+    }
+}
---
+++ basic_c_sharp_projects/ClassMethodExercise4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodExercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.stuff(3, 324);
+            class1.stuff(first: 3, second: 34);
+            Console.ReadLine();
+        }
+    }
+}
+
+//1. Create a class. In that class, create a void method that takes two integers as 
+//parameters.Have the method do a math operation on the first integer and display the second integer to the screen.
+
+//2. In the Main() method of the console app, instantiate the class.
+
+//3. Call the method in the class, passing in two numbers.
+
+//4. Call the method in the class, specifying the parameters by name.
