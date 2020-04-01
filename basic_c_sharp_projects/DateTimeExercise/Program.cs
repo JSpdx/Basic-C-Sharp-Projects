82854f715783a3b5f0a311a95d62b16ed44119c0
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-04-01 16:25:59 -0700

    ad DateTimeExercise

---
+++ basic_c_sharp_projects/DateTimeExercise/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            //this program prints the current time, asks the user for a number, then prints
+            //the time after that many hours have passed.
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please enter a number");
+            double num = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(DateTime.Now.AddHours(num));
+            Console.ReadLine();
+
+        }
+}
+}
