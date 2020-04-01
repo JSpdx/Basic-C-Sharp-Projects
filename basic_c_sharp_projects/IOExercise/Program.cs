b9d38d86c4e7519610d70b5d38a0256504509c7a
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-04-01 15:56:13 -0700

    add IOExercise

---
+++ basic_c_sharp_projects/IOExercise/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IOExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to log");
+            string num = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Jon\OneDrive\Desktop\Github Repos\Basic-C-Sharp-Projects\basic_c_sharp_projects\IOExercise\TextFile1.txt", num);
+            Console.WriteLine(File.ReadAllText(@"C:\Users\Jon\OneDrive\Desktop\Github Repos\Basic-C-Sharp-Projects\basic_c_sharp_projects\IOExercise\TextFile1.txt"));
+            Console.Read();
+
+        }
+    }
+}
