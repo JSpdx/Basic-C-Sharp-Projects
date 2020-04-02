97b7cf3aa82ae211a9b545640d93b0be4a8a1add
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-04-02 14:21:25 -0700

    add exception handling2 looping still not working right

---
+++ basic_c_sharp_projects/ExceptionHandling2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isValid = false;
+            int age = 0;
+            while (!isValid)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter your age:\n");
+                    isValid = int.TryParse(Console.ReadLine(), out age);
+                    if (!isValid) Console.WriteLine("Please enter a valid age.");
+                    
+                    DateTime date = DateTime.Now;
+                    TimeSpan years = new TimeSpan(age * 365, 0, 0, 0, 0);
+                    DateTime yearBorn = date - years;
+                    Console.WriteLine("You were born in the year {0}", yearBorn.Year);
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("The number you entered was too high");
+                    
+                }
+
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
